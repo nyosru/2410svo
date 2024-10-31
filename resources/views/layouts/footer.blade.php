39635873d8b701f6b01cae354a203aa3a00ac2f4
Sergey <1@php-cat.com>
2024-10-31 21:08:39 +0500

    1-3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,10 @@
-<footer>
-{{--    <!-- Информация о копирайте -->--}}
-    © {{ date('Y') }} My Company1
+<footer class="sm:flex items-center justify-between h-24 bg-gray-100 border-t border-gray-300">
+    <div class="flex-1 flex items-center justify-center border-r border-gray-300 text-center">
+        Все права защищены © {{ date('Y') }}<br/>
+        Лучше не нарушать
+    </div>
+    <div class="flex-1 flex items-center justify-center">
+        Создание сайта: <a href="https://php-cat.com" target="_blank"
+                           class="underline text-blue-600 p-2 hover:bg-yellow-200" title="php-cat.com">php-cat.com</a>
+    </div>
 </footer>
