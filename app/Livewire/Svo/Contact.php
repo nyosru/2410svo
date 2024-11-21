2d4a9f68829827f670bd7f4e13e34405727ef2b9
Sergey <1@php-cat.com>
2024-11-21 23:06:33 +0500

    17-6

--- app/Livewire/Svo/Contact.php
+++ app/Livewire/Svo/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Svo;
 
+use App\Models\ShopItem;
 use App\Models\SvoContact;
 use Livewire\Component;
 
@@ -9,6 +10,7 @@ class Contact extends Component
 {
 
     public $data; // Добавляем свойство для хранения текста поиска
+    public $search;
 
     public function mount()
     {
@@ -17,6 +19,34 @@ class Contact extends Component
 
     public function render()
     {
+
+        // Разделяем поисковый запрос на слова
+        $keywords = explode(' ', trim($this->search));
+
+        // Формируем регулярное выражение для поиска
+        $searchPattern = implode('.*', array_map(function ($keyword) {
+            return preg_quote($keyword, '/');
+        }, $keywords));
+
+        // Базовый запрос
+        $query = SvoContact::with('photoLoaded');
+
+        // Фильтр по поисковому запросу
+        if (!empty($this->search)) {
+            $query->where('gruppa', 'REGEXP', $searchPattern)
+                ->orWhere('m_o_l', 'REGEXP', $searchPattern)
+                ->orWhere('yur_adres', 'REGEXP', $searchPattern);
+        }
+
+        // Фильтр по выбранным фирмам
+//        if (!empty($this->selectedFirms)) {
+//            $query->whereIn('firma', $this->selectedFirms);
+//        }
+
+        // Выполняем запрос с пагинацией
+//        $this->data = $query->paginate(10);
+        $this->data = $query->get();
+
         return view('livewire.svo.contact');
     }
 }
--- resources/views/livewire/svo/contact.blade.php
+++ resources/views/livewire/svo/contact.blade.php
@@ -1,8 +1,16 @@
 <div>
 
-    @foreach ($data as $с )
-        <livewire:svo.contact-item :item="$с" :key="$с->id"/>
-    @endforeach
+        <div class="w-[600px] mx-auto my-5">
+    Поиск: <input wire:model.live="search" type="text" class="w-[400px] border border-2 p-1"/>
+        </div>
+
+    @if( count($data) == 0 )
+        <div class="bg-yellow-300 p-2 w-[600px] mx-auto">Нет контактов для показа ( Поиск: {{ $search ?? '' }} )</div>
+    @else
+        @foreach ($data as $с )
+            <livewire:svo.contact-item :item="$с" :key="$с->id"/>
+        @endforeach
+    @endif
 
 {{--    <div class="my-6">--}}
 {{--        {{ $data1->links($paginationView) }}--}}
